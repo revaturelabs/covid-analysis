693eab3e8de0c95c5ed2fe48ac18f3eeb513b404
Myung Jin Song <jsong220@gmail.com>
2021-02-03 23:44:28 -0800

    init failing test and method templates

---
+++ twitter-general/tweet-positive-negative/src/main/scala/SentimentRatio.scala
@@ -0,0 +1,19 @@
+object SentimentRatio {
+  //initialize NLP library sentiment analysis pipeline
+  def setUpPipeline {}
+
+  //Basic math to determine ratio of positve to negative tweets
+  def positiveRatio(positiveSentiment: Int, negativeSentiment: Int): Int = {
+    43
+  }
+
+  /** check twitter text fields to determine
+    * if tweet is net positive or negative sentiment.
+    * Returns an int representing total number of
+    * negative or positive tweets
+    * TODO: should take in dataframe
+    */
+  def analyzeSentiments: Int = {
+    0
+  }
+}
