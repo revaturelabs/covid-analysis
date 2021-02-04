89b5bea6e76dde0eea49b24ad1b6debfd620f2a4
My Lord <page.c.tyler@gmail.com>
2021-02-04 11:00:18 -0500

    added testing suite to covid response app.

---
+++ econ-group/covid-response/src/test/scala/CovidResponseSpecs.scala
@@ -0,0 +1,9 @@
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.functions._
+import com.github.mrpowers.spark.fast.tests.DatasetComparer
+import org.scalatest.funspec
+
+class CovidResponseSpecs extends FunSpec with SparkSessionTestWrapper with DatasetComparer {
+
+}
