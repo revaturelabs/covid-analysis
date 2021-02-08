9e0c9bebd1bfc81818cb688a4bd46acd59edb5af
spark131008 <spark131008@gmail.com>
2021-02-08 16:58:04 -0500

    updated

--- stockmarket/src/main/scala/com/revature/scalawags/project3/stockmarket/WeeklyChangeRunner.scala
+++ stockmarket/src/main/scala/com/revature/scalawags/project3/stockmarket/WeeklyChangeRunner.scala
@@ -12,58 +12,60 @@ import org.apache.spark.sql.{DataFrame, Dataset}
 
 object WeeklyChangeRunner{
     def main(args: Array[String]): Unit = {
-    Logger.getLogger("org").setLevel(Level.ERROR)
+        Logger.getLogger("org").setLevel(Level.ERROR)
 
-    val spark = SparkSession.builder()
-       .appName("weekly_Chnage")
-       .master("local[4]")
-       .getOrCreate()
+        val spark = SparkSession.builder()
+            .appName("weekly_Chnage")
+            .master("local[4]")
+            .getOrCreate()
 
-    import spark.implicits._
+        import spark.implicits._
 
-    val africaDF = spark
-    .read
-    .option("inferSchema", "true")
-    .option("header", "true")
-    .csv("/datalake/datalake/Africa_*.csv").cache()
+        val africaDF = dataFrameByRegion(spark, "Africa")
+        val dateFormatDF = dateColumnFormating(africaDF)
+        //dateFormatDF.show(1800, false)
 
-    africaDF.show(2000, false)
 
-    val dateFormat1 = africaDF.withColumn("New Date", date_format(to_date($"Date", "MMM dd, yyyy"),"MM/dd/yyyy")).select($"New Date", $"Open")
-    val dataFormat2 = africaDF.withColumn("New Date", date_format(to_date($"Date", "MM/dd/yyyy"), "MM/dd/yyyy")).select($"New Date", $"Open")
-
-    val date_f1 = dateFormat1.as("df_form1")
-    val date_f2 = dataFormat2.as("df_form2")
-
-    val joined_df = date_f1.join(
-        date_f2
-        , col("df_form1.New Date") === col("df_form2.New Date")
-        , "outer")
-    joined_df.show(2000, false)
-
-    val asiaDF = dataFrameByRegion(spark, "Asia")
-    dfCalcuation(asiaDF)
-
+        // val asiaDF = dataFrameByRegion(spark, "Asia")
+        // val sumDF = sumOfOpenPrice(asiaDF)
+        // sumDF.show(2000, false)
     }
+
     def dataFrameByRegion(spark: SparkSession, region: String): DataFrame = {
         val regionDF = spark
         .read
         .option("inferSchema", "true")
         .option("header", "true")
         .csv(s"/datalake/datalake/${region}_*.csv").cache()
-    
         return regionDF
     }
 
-    def dfCalcuation(df: DataFrame){
-        //df.printSchema
-        // df.show(200, false)
+    def sumOfOpenPrice(df: DataFrame): DataFrame = {
         val dataFormatChangeDF = df.withColumn("Date", to_date(col("Date"), "MM/dd/yyyy"))
         val removeCommaDF = dataFormatChangeDF.withColumn("Open", regexp_replace(col("Open"), ",", ""))
         val dataframe = removeCommaDF.withColumn("Open", col("Open").cast(DoubleType)).select("Date", "Open")
         val sumDF = dataframe.groupBy(col("Date")).agg(sum(col("Open"))).orderBy(desc("Date"))
-        sumDF.printSchema
-        sumDF.show(2000, false)
+        return sumDF
     }
-    
+
+    def dateColumnFormating(df: DataFrame){
+        val wordDateFormat = df.withColumn("Word_Date", to_date(col("Date"), "MMM dd, yyyy")).select(col("Word_Date"), col("Open"))
+        // val dataFormat2 = df.withColumn("New Date", date_format(to_date(col("Date"), "MM/dd/yyyy"), "MM/dd/yyyy")).select("New Date", "Open")
+        //wordDateFormat.printSchema
+        //return wordDateFormat1
+
+        val numDataFormat = df.withColumn("Num_Date", to_date(col("Date"), "MM/dd/yyyy")).select(col("Date"), col("Open"))
+        //numDataFormat.printSchema
+        return numDataFormats
+
+        // val date_f1 = dateFormat1.as("df_form1")
+        // val date_f2 = dataFormat2.as("df_form2")
+
+        // val joined_df = date_f1.join(
+        // date_f2
+        // , col("df_form1.New Date") === col("df_form2.New Date")
+        // , "outer")
+        // joined_df.show(2000, false)
+
+    }
 }
