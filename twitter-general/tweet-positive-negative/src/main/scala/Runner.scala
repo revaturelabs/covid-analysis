51b99e28e8448fa167e3c8c99b387c1aa333ba7a
Myung Jin Song <jsong220@gmail.com>
2021-02-12 11:00:31 -0800

    Add functionality to methods

--- twitter-general/tweet-positive-negative/src/main/scala/Runner.scala
+++ twitter-general/tweet-positive-negative/src/main/scala/Runner.scala
@@ -1,6 +1,77 @@
-object Runner {
-    def main(args: Array[String]): Unit = {
-        
-    }
-}
+import org.apache.spark.sql.{SparkSession, DataFrame}
+import org.apache.hadoop.fs._
+import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
+import scala.sys.process._
 
+object Runner {
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession
+      .builder()
+      .appName("sentimentAnalysis")
+      .master("local[*]")
+      .getOrCreate()
+
+    //Connect to s3
+    val accessKeyId = sys.env("AWS_ACCESS_KEY_ID")
+    val secretAccessKey = sys.env("AWS_SECRET_ACCESS_KEY")
+    val s3Endpoint = "s3a://covid-analysis-p3"
+    spark.sparkContext.hadoopConfiguration
+      .set("fs.s3n.awsAccessKeyid", accessKeyId)
+    spark.sparkContext.hadoopConfiguration
+      .set("fs.s3n.awsSecretAccessKey", secretAccessKey)
+
+    //grabbing date ranges for analysis
+    def getdates: String = {
+      val inputFile = args(0).toInt
+      inputFile match {
+        case 1 =>
+          val textFile = "dec_11-dec_25/*"
+          textFile
+        case 2 =>
+          val textFile = "dec_26-jan_05/*"
+          textFile
+        case 3 =>
+          val textFile = "feb_03-feb_14/*"
+          textFile
+        case 4 =>
+          val textFile = "test-data/*"
+          textFile
+        case _ =>
+          val textFile = "not valid input.."
+          textFile
+      }
+    }
+
+    val dateRange = getdates
+    val results = SentimentRatio.tweetPositiveNegative(
+      spark,
+      s"$s3Endpoint/datalake/twitter-general/$dateRange"
+    )
+    //save results to local as csv and rename for more concise file name 
+    //https://stackoverflow.com/questions/41990086/specifying-the-filename-when-saving-a-dataframe-as-a-csv
+    val fs = FileSystem.get(spark.sparkContext.hadoopConfiguration)
+    results
+      .coalesce(1)
+      .write
+      .mode("overwrite")
+      .option("header", "false")
+      .csv("resultsTesting")
+    val file =
+      fs.globStatus(new Path("resultsTesting/part*"))(0).getPath().getName()
+
+    fs.rename(
+      new Path("resultsTesting/" + file),
+      new Path("resultsTesting/testResults.csv")
+    )
+    fs.delete(new Path("testResults.csv-temp"), true)
+
+    //to send copy file command to s3
+    //https://medium.com/@mcamara89/executing-shell-commands-from-scala-7001f8868128
+    val sendToS3 =
+      s"aws s3 cp resultsTesting/testResults.csv s3://covid-analysis-p3/datawarehouse/twitter-general/sentiment-analysis/testResults.csv"
+    sendToS3.!
+
+    spark.close()
+  }
+}
--- twitter-general/tweet-positive-negative/src/main/scala/SentimentRatio.scala
+++ twitter-general/tweet-positive-negative/src/main/scala/SentimentRatio.scala
@@ -2,13 +2,14 @@ import com.johnsnowlabs.nlp.pretrained.PretrainedPipeline
 import com.johnsnowlabs.nlp.SparkNLP
 import org.apache.spark.sql.{SparkSession, DataFrame}
 import org.apache.spark.sql.functions.{array_contains, col}
+import scala.math.BigDecimal.RoundingMode
 
 object SentimentRatio {
 
   //grabs tweets from text file and splits each tweet into a separate row
-  def readTweets(spark: SparkSession): DataFrame = {
+  def readTweets(filePath: String, spark: SparkSession): DataFrame = {
     import spark.implicits._
-    val rawText = spark.read.text("sample-data/test-data.txt")
+    val rawText = spark.read.text(filePath)
     val tweets = rawText
       .map(x => {
         val elements = x.getString(0).split("\"text\":")
@@ -22,7 +23,11 @@ object SentimentRatio {
 
   //initialize JohnSnowLabs Spark NLP library pretrained sentiment analysis pipeline
   def setUpPipeline(df: DataFrame): DataFrame = {
-    df
+    val pipeline =
+      PretrainedPipeline("analyze_sentimentdl_use_twitter", lang = "en")
+    val results = pipeline.annotate(df, "value")
+    //make it work with $"col1"
+    results
   }
 
   /** check twitter text fields to determine
@@ -30,21 +35,79 @@ object SentimentRatio {
     * Returns dataframe with counts of each sentiment.
     */
   def analyzeSentiment(spark: SparkSession, df: DataFrame): DataFrame = {
-    df
+    import spark.implicits._
+    val sentimentResults = df
+      .select($"sentiment" ("result").as("Sentiment Results"))
+      .groupBy($"Sentiment Results")
+      .count()
+      .orderBy($"count".desc)
+      .cache()
+
+    sentimentResults
   }
 
   //selects and returns count of only positive sentiments
   def positiveSentiments(spark: SparkSession, df: DataFrame): Int = {
-    43
+    import spark.implicits._
+    val positiveSentiment = df
+      .where(array_contains(($"Sentiment Results"), "positive"))
+      .collect()
+
+    // println(positiveSentiment(0)(1))
+    positiveSentiment(0)(1).toString.toInt
   }
 
   //selects and returns count of only negative sentiments
   def negativeSentiments(spark: SparkSession, df: DataFrame): Int = {
-    43
+    import spark.implicits._
+    val negativeSentiment = df
+      .where(array_contains(($"Sentiment Results"), "negative"))
+      .collect()
+
+    // println(negativeSentiment(0)(1))
+    negativeSentiment(0)(1).toString.toInt
   }
 
   //Returns percentage of positive to negative tweets with two decimal precision
   def positiveRatio(posSentiment: Int, negSentiment: Int): Double = {
-    43.00
+    val posRatio =
+      posSentiment.toDouble / (posSentiment.toDouble + negSentiment.toDouble)
+    val posPercent =
+      BigDecimal(posRatio).setScale(2, RoundingMode.HALF_UP).toDouble
+    // println(posPercent)
+    posPercent * 100
+  }
+
+  //converts final results into a dataframe
+  def createResultsDf(
+      spark: SparkSession,
+      negResults: Int,
+      posResults: Int,
+      finalResults: Int
+  ): DataFrame = {
+    import spark.implicits._
+    val resultsDF = spark
+      .createDataFrame(
+        Seq(
+          ("Number of Negative Sentiments", negResults),
+          ("Number of Positive Sentiments", posResults),
+          ("Percentage of Positive Sentiments", finalResults)
+        )
+      )
+      .toDF()
+
+    resultsDF
+  }
+
+  //runs all methods above
+  def tweetPositiveNegative(spark: SparkSession, path: String): DataFrame = {
+    val data = readTweets(path, spark)
+    val df = setUpPipeline(data)
+    val sentiments = analyzeSentiment(spark, df)
+    val negSent = negativeSentiments(spark, sentiments)
+    val posSent = positiveSentiments(spark, sentiments)
+    val finalPosRatio = positiveRatio(posSent, negSent).toInt
+
+    createResultsDf(spark, negSent, posSent, finalPosRatio)
   }
 }
