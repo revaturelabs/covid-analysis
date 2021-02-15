e2afaf77b3a94ad35117b23c80e419ef783c3628
Chris Johnson <cjohn281@outlook.com>
2021-02-15 15:06:43 -0500

    Commented out Spark-AWS configuration

--- twitter-covid/related-hashtags/src/main/scala/main/Main.scala
+++ twitter-covid/related-hashtags/src/main/scala/main/Main.scala
@@ -25,12 +25,20 @@ object Main {
     val spark = SparkSession
       .builder()
       .appName("Related-Hashtags")
-      .master("yarn")               // Change "yarn" to "local[*]" if running the applicaiton locally.
+      .master("yarn")               // Change "yarn" to "local[*]" if running the application locally.
       .getOrCreate()
+
+    // Load in AWS credientials from environment, uncomment these lines if running locally
+    // spark.sparkContext.hadoopConfiguration.set("fs.s3a.awsAccessKeyId", sys.env("AWS_ACCESS_KEY_ID"))
+    // spark.sparkContext.hadoopConfiguration.set("fs.s3a.awsSecretAccessKey", sys.env("AWS_SECRET_ACCESS_KEY"))
+    // spark.sparkContext.hadoopConfiguration.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
 
     // twitterDF is the base DataFrame created from the contents of an input json file.
     val twitterDF = FileUtil.getDataFrameFromJson(spark, jsonPath)
 
     RelatedHashtags.getHashtagsWithCovid(spark, twitterDF)
-    }
+
+    spark.stop
   }
+}
+    
