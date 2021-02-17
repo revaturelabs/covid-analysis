b7e39b743e1d05545d40d1bf85510d9a9d2c01f3
Chris Johnson <cjohn281@outlook.com>
2021-02-17 11:05:59 -0500

    Updated linear regression comments

--- twitter-covid/twitter-covid-us/src/main/scala/TwitterCovidAnalysis.scala
+++ twitter-covid/twitter-covid-us/src/main/scala/TwitterCovidAnalysis.scala
@@ -104,7 +104,11 @@ object TwitterCovidAnalysis {
   }
 
   /** Helper method for twitterVolumeSpikes()
-    * Uses Spark ML to perform linear regression
+    * Uses Spark ML to perform linear regression.
+	  * 
+	  * This method is currently not in used in the project, but may prove
+	  * useful for analysis in the future.  It is intended to be used on
+	  * the DataFrame that is returned by the twitterCovidAnalysis method.
     *
     * @param df
     */
